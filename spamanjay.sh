4e3d19d71cb2ea3269874fa6964a87f637166069
wkwkwk <wkwkwk>
2021-06-08 23:59:52 +0000

    fill bytes

---
+++ spamanjay.sh
@@ -0,0 +1,56 @@
+touch "hehehe.txt"
+echo "spam " >> "hehehe.txt"
+git pull origin --all
+git add .
+git commit -m "fill bytes"
+git push --force origin --all
+touch "hehehe.txt"
+echo "spam " >> "hehehe.txt"
+git pull origin --all
+git add .
+git commit -m "fill bytes"
+git push --force origin --all
+touch "hehehe.txt"
+echo "spam " >> "hehehe.txt"
+git pull origin --all
+git add .
+git commit -m "fill bytes"
+git push --force origin --all
+touch "hehehe.txt"
+echo "spam " >> "hehehe.txt"
+git pull origin --all
+git add .
+git commit -m "fill bytes"
+git push --force origin --all
+touch "hehehe.txt"
+echo "spam " >> "hehehe.txt"
+git pull origin --all
+git add .
+git commit -m "fill bytes"
+git push --force origin --all
+touch "hehehe.txt"
+echo "spam " >> "hehehe.txt"
+git pull origin --all
+git add .
+git commit -m "fill bytes"
+git push --force origin --alltouch "hehehe.txt"
+echo "spam " >> "hehehe.txt"
+git pull origin --all
+git add .
+git commit -m "fill bytes"
+git push --force origin --all
+touch "hehehe.txt"
+echo "spam " >> "hehehe.txt"
+git pull origin --all
+git add .
+git commit -m "fill bytes"
+git push --force origin --all
+touch "hehehe.txt"
+echo "spam " >> "hehehe.txt"
+git pull origin --all
+git add .
+git commit -m "fill bytes"
+git push --force origin --all
+printf "Ditunggu..."
+sleep 1200
+start spamanjay.sh
