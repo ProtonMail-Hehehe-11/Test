dab3e977e0a767274cdc349dcb72c5cec277da41
wkwkwk <wkwkwk>
2021-06-09 00:07:34 +0000

    fill bytes

--- spamanjay.sh
+++ spamanjay.sh
@@ -4,53 +4,77 @@ git pull origin --all
 git add .
 git commit -m "fill bytes"
 git push --force origin --all
+sleep 120
 touch "hehehe.txt"
 echo "spam " >> "hehehe.txt"
 git pull origin --all
 git add .
 git commit -m "fill bytes"
 git push --force origin --all
+sleep 120
 touch "hehehe.txt"
 echo "spam " >> "hehehe.txt"
 git pull origin --all
 git add .
 git commit -m "fill bytes"
 git push --force origin --all
+sleep 120
 touch "hehehe.txt"
 echo "spam " >> "hehehe.txt"
 git pull origin --all
 git add .
 git commit -m "fill bytes"
 git push --force origin --all
+sleep 120
 touch "hehehe.txt"
 echo "spam " >> "hehehe.txt"
 git pull origin --all
 git add .
 git commit -m "fill bytes"
 git push --force origin --all
-touch "hehehe.txt"
-echo "spam " >> "hehehe.txt"
-git pull origin --all
-git add .
-git commit -m "fill bytes"
-git push --force origin --alltouch "hehehe.txt"
-echo "spam " >> "hehehe.txt"
-git pull origin --all
-git add .
-git commit -m "fill bytes"
-git push --force origin --all
+sleep 120
 touch "hehehe.txt"
 echo "spam " >> "hehehe.txt"
 git pull origin --all
 git add .
 git commit -m "fill bytes"
 git push --force origin --all
+sleep 120
 touch "hehehe.txt"
 echo "spam " >> "hehehe.txt"
 git pull origin --all
 git add .
 git commit -m "fill bytes"
 git push --force origin --all
+sleep 120
+touch "hehehe.txt"
+echo "spam " >> "hehehe.txt"
+git pull origin --all
+git add .
+git commit -m "fill bytes"
+git push --force origin --all
+sleep 120
+touch "hehehe.txt"
+echo "spam " >> "hehehe.txt"
+git pull origin --all
+git add .
+git commit -m "fill bytes"
+git push --force origin --all
+sleep 120
+touch "hehehe.txt"
+echo "spam " >> "hehehe.txt"
+git pull origin --all
+git add .
+git commit -m "fill bytes"
+git push --force origin --all
+sleep 120
+touch "hehehe.txt"
+echo "spam " >> "hehehe.txt"
+git pull origin --all
+git add .
+git commit -m "fill bytes"
+git push --force origin --all
+sleep 120
 printf "Ditunggu..."
 sleep 1200
 start spamanjay.sh
